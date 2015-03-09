13b2b997818d1a1393cffc27d81f525b2e139d78
Gilbert Pellegrom <gilbert@pellegrom.me>
2015-03-09 15:22:30 +0000

    Create the RSS feed

--- src/BlogRss.php
+++ src/BlogRss.php
@@ -6,7 +6,25 @@ class BlogRss extends Plugin {
 
 	public function init()
 	{
-		echo 'Hello World';
+		$this->events->addListener('baun.filesToPosts', [$this, 'feed']);
+
+		$this->theme->addPath(__DIR__ . '/templates');
+	}
+
+	public function feed($event, $posts)
+	{
+		$posts = array_slice($posts, 0, $this->config->get('blog.posts_per_page'));
+		foreach ($posts as $key => $post) {
+			$posts[$key]['published'] = date('r', strtotime($post['published']));
+		}
+
+		$this->router->add('GET', '/feed', function() use ($posts) {
+			header('Content-Type: application/rss+xml; charset=utf-8');
+			return $this->theme->render('feed', [
+				'base_url' => $this->config->get('app.base_url'),
+				'posts' => $posts
+			]);
+		});
 	}
 
 }
